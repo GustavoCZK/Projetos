42c897342cf5a5189700ea4a6ba4428fa73f5e2b
Gustavo Conte <402497@unoesc.lan>
2023-08-10 22:05:26 -0300

    10/08

---
+++ PSIU/PSIUWeb/Models/Psico.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace PSIUWeb.Models
+{
+    public class Psico
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nome Requerido.")]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "CPR Requerido.")]
+        [Display(Name = "CRP")]
+        public string CRP { get; set; }
+        [Required(ErrorMessage = "Release Requerido.")]
+        [Display(Name = "Released")]
+        public bool Released { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+        //navigation propert
+    }
+}
