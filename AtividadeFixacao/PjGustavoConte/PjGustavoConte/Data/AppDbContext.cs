b6111eb449b5bcac361a9d4f90912a41fe24eb90
Gustavo Conte <402497@unoesc.lan>
2023-08-31 19:25:56 -0300

    AtvFix

---
+++ AtividadeFixacao/PjGustavoConte/PjGustavoConte/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PjGustavoConte.Models;
+
+namespace PjGustavoConte.Data
+{
+    public class AppDbContext :
+        IdentityDbContext<AppPsico>
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Psicologo>? Psicologos { get; set; }
+
+    }
+}
---
+++ AtividadeFixacao/PjGustavoConte/PjGustavoConte/Models/Psicologo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PjGustavoConte.Models
+{
+    public class Psicologo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome requerido.")]
+        [Display(Name = "Nome")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Data de nascimento requerida.")]
+        [Display(Name = "Data de nascimento")]
+        public DateTime BirthDate { get; set; }
+
+        [ForeignKey("Psico")]
+        public string PsicoId { get; set; }
+        public AppPsico Psico { get; set; }
+    }
+}
