95805be7b780357fae7a8624c02284bb8f346e17
Gustavo Conte <402497@unoesc.lan>
2023-09-28 22:01:56 -0300

    FullPacient

--- PSIU-main/PSIUWeb/Controllers/PacientController.cs
+++ PSIU-main/PSIUWeb/Controllers/PacientController.cs
@@ -9,7 +9,7 @@ namespace PSIUWeb.Controllers
     {
         private IPacientRepositor pacientRepositor;
 
-        public  PacientController(IPacientRepositor _pacientRepo)
+        public PacientController(IPacientRepositor _pacientRepo)
         {
             pacientRepositor = _pacientRepo;
         }
@@ -36,19 +36,68 @@ namespace PSIUWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(Pacient pacient)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 try
                 {
                     pacientRepositor.Update(pacient);
+                    return View("Index", pacientRepositor.GetPacients());
                 }
                 catch (DbUpdateConcurrencyException)
                 {
                     throw;
                 }
-          
+
             }
             return View("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Pacient? p = pacientRepositor.GetPacientById(id.Value);
+
+            if (p == null)
+                return NotFound();
+
+            return View(p);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteComfirmed(int id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            pacientRepositor.Delete(id);
+
+            return RedirectToAction( nameof(Index) );
+
+        }
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Insert(Pacient p)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    pacientRepositor.Create(p);
+                    return View("Index", pacientRepositor.GetPacients());
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+            return View();
         }
     }
 }
